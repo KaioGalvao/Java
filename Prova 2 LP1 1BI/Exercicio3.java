d23afc349020a0b82a857e0092643e139b40e8e7
kaio galvao <84183505+KaioGalvao@users.noreply.github.com>
2021-10-24 00:52:48 -0300

    Add files via upload

---
+++ Prova 2 LP1 1BI/Exercicio3.java
@@ -0,0 +1,8 @@
+public class Exercicio3{
+    public static void main(String[] args) {
+        for(int i = 1; i <= 200; i++){
+            if(i % 2 == 0)
+            System.out.println(i);
+        }    
+    }
+}
