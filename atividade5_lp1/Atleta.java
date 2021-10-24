85bf3d55d998c2ceaef9183c898ecf229de48e0a
kaio galvao <84183505+KaioGalvao@users.noreply.github.com>
2021-10-24 01:29:17 -0300

    Add files via upload

---
+++ atividade5_lp1/Atleta.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+public class Atleta extends Aluno {
+    private String modalidade;
+
+    public Atleta(){
+        super();
+        this.modalidade = "";
+    }
+
+    public Atleta(String nome, LocalDate nascimento, String prontuario, String email, String modalidade){
+        super(nome, nascimento, prontuario, email);
+        this.modalidade = modalidade;
+    }
+
+    public String getModalidade() {
+        return modalidade;
+    }
+    public void setModalidade(String modalidade) {
+        this.modalidade = modalidade;
+    }
+
+    public String toString() {
+
+        return super.toString() + " [modalidade: " + this.modalidade + " ]";
+    }
+}
